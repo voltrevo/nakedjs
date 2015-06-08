ff840c0f6a03390dd5a5ce74306588fcd8eafe69
Andrew Morris <voltrevo@gmail.com>
2015-06-08 13:00:31 +1000

    cli now takes -p/--port option and prints url

--- src/cli.js
+++ src/cli.js
@@ -2,14 +2,19 @@
 
 'use strict'
 
-if (process.argv.length !== 3) {
-  console.error('Usage: nakedjs <file>')
-  process.exit(1)
+var argv = require('minimist')(process.argv.slice(2))
+
+if (argv._.length < 1) {
+  console.error('Usage: nakedjs <file> [opts]')
 }
 
 var http = require('http')
 var nakedjs = require('./index')
 
+var port = argv.p || argv.port || 8080
+
 http
-  .createServer(nakedjs(process.argv[2]))
-  .listen(8080)
+  .createServer(nakedjs(argv._[0]))
+  .listen(port)
+
+console.log('Server running at http://localhost:' + port + '/')
